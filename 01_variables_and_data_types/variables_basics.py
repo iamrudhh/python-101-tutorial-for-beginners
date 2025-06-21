71222898d96d012a7af0b1800590740bb49965b5
Anirudh.M <103243485+iamrudhh@users.noreply.github.com>
2025-06-21 22:24:03 +0530

    Update variables_basics.py

--- 01_variables_and_data_types/variables_basics.py
+++ 01_variables_and_data_types/variables_basics.py
@@ -1,1 +1,32 @@
+# keeping one name
+name = "cristano"
+print(name)
+
+# count of letters (just putting it as a string now)
+name_length = "8"
+print(name_length)
+
+# checking what type the name and length are
+print(type(name))
+print(type(name_length))
+
+# keeping some items one after another
+tuple = ("one", "two", "three")
+print(type(tuple))
+
+# group of values (not in order)
+dictionary = {"one", "two", "three"}
+print(type(dictionary))
+
+# true or false value
+boolean = True
+print(type(boolean))
+
+# numbers from 0 to 9
+range = range(10)
+print(type(range))
+
+# letters in byte format
+bytes = b"name"
+print(type(bytes))
 
