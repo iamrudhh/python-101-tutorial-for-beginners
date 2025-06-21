20c6cd5b0511c54cb2e7a7dabf2cdcf3826a69af
Anirudh.M <103243485+iamrudhh@users.noreply.github.com>
2025-06-21 22:27:57 +0530

    Create number_types.py

---
+++ 02_numbers/number_types.py
@@ -0,0 +1,25 @@
+# normal number
+number_one = 1
+print(type(number_one))
+
+# number with dot
+number_two = 1.0
+print(type(number_two))
+
+# number with j (for special math use)
+number_three = 3.14j
+print(type(number_three))  
+
+# make any number positive
+print(abs(+8))
+
+# round off the number
+print(round(1.4))
+print(round(1.5))
+print(round(1.6))
+
+# change number to binary
+print(bin(8))
+
+# change number to hex
+print(hex(8))
